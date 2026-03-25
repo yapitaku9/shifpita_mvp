51fcbfafda6ff58c37e72cd90a597a299e9e484b
yapitaku9 <yapitaku9@gmail.com>
2026-03-26 07:21:21 +0900

    シフト計算時間を２０分に

--- app/services/generator.py
+++ app/services/generator.py
@@ -852,7 +852,7 @@ class ShiftGenerator:
         logging.info("Solving problem...")
         prob += pulp.lpSum(objective_terms), "Objective"
         prob.writeLP("ShiftProblem.lp")  # デバッグ用
-        solver = pulp.PULP_CBC_CMD(msg=True, logPath="solver.log", timeLimit=600)
+        solver = pulp.PULP_CBC_CMD(msg=True, logPath="solver.log", timeLimit=1200)
         status = prob.solve(solver)
         logging.info(f"Solver finished with status: {pulp.LpStatus[status]}")
 
